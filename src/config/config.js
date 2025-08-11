122846207733870f944ec13c36e7d3bed99ff25d
Van Phuc <phuc04nd@gmail.com>
2025-08-11 13:46:26 +0700

    fix db

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.HOSTNAME || 'localhost',
+    host: process.env.DB_HOST || 'localhost',
     dialect: process.env.DB_DIALECT
   },
   test: {
--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 exports.pool = mysql.createPool({
-    host: process.env.HOSTNAME || 'localhost',
+    host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || '',
