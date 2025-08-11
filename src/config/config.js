5cccb0bca9e87f4ea6b25d9fbe7faf5a6a323eca
Van Phuc <phuc04nd@gmail.com>
2025-08-11 14:07:49 +0700

    fix db

--- src/config/config.js
+++ src/config/config.js
@@ -12,6 +12,10 @@ module.exports = {
 
   },
   production: {
-
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || 'localhost',
+    dialect: process.env.DB_DIALECT || 'mysql'
   }
 };
