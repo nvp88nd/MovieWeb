5715bc27d2c634cf247d4c8decd97925fcc07c09
Van Phuc <phuc04nd@gmail.com>
2025-08-23 18:54:40 +0700

    db

--- src/config/config.js
+++ src/config/config.js
@@ -16,6 +16,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || 'mysql'
   }
 };
