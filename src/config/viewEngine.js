1af6231adf0e146c4a0c46d5943f150f76463770
Van Phuc <phuc04nd@gmail.com>
2025-08-23 19:48:05 +0700

    fix config ejs

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -3,8 +3,8 @@ const express = require('express');
 
 const configViewEngine = (app) => {
     app.set('view engine', 'ejs');
-    app.set('views', path.join(__dirname, '..', 'src', 'views'));
-    app.use(express.static(path.join(__dirname, '..', 'src', 'public')));
+    app.set('views', path.join(__dirname, '..', 'views'));
+    app.use(express.static(path.join(__dirname, '..', 'public')));
 }
 
 module.exports = configViewEngine;
